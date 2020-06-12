1ddec52bbbb005298d4fb0febbc50887786f9948
manojsid <siddammanoj1998@gmail.com>
2020-06-12 20:37:13 +0530

    Coin is flipped

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 echo "Welcome to flipcoin problem statement"
+
+Heads=0;
+Tails=1;
+Flip=$(($RANDOM%2))
+if [[ $Flip -eq $Heads ]]; then
+	#statements
+	echo "Coin flipped to Heads"
+else
+	echo "Coin flipped to Tails"
+fi
